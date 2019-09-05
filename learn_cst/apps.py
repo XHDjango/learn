206169307427981fe35f2fc4f581c137d5403a29
lamborghini1993 <1323242382@qq.com>
2019-09-05 16:09:36 +0800

    init learn_cst cookie session token

---
+++ learn_cst/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LearnCstConfig(AppConfig):
+    name = 'learn_cst'
--- learn_django/urls.py
+++ learn_django/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('learn_view/', include("learn_view.urls")),
+    path('learn_cst/', include("learn_cst.urls")),
 ]
