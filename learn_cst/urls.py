9e1e3829db869165ac57f0dee3e547d79fa333df
lamborghini1993 <1323242382@qq.com>
2019-09-05 17:40:49 +0800

    change name

--- learn_cst/urls.py
+++ learn_cst/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 app_name = "cst"
 urlpatterns = [
-    path('c_login', views.cookie_login, name="login"),
-    path('c_logout', views.cookie_logout, name="logout"),
-    path('user', views.user, name="user"),
+    path('c_user', views.cookie_user, name="c_user"),
+    path('c_login', views.cookie_login, name="c_login"),
+    path('c_logout', views.cookie_logout, name="c_logout"),
+
+    # path('s_user', views.session_user, name="s_user"),
+    # path('s_login', views.session_login, name="s_login"),
+    # path('s_logout', views.session_logout, name="s_logout"),
 ]
--- learn_cst/views.py
+++ learn_cst/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-COOKIE_KEYNAME = "name"
+COOKIE_KEYNAME = "c_name"
+SESSION_KEYNAME = "s_name"
 
 
-def user(request):
+def cookie_user(request):
+    # request.get_signed_cookie(COOKIE_KEYNAME, salt="salt")
     username = request.COOKIES.get(COOKIE_KEYNAME)
     if username:
-        return render(request, "user.html", context=locals())
-    return redirect(reverse("cst:login"))
+        return render(request, "c_user.html", context=locals())
+    return redirect(reverse("cst:c_login"))
 
 
 def cookie_login(request):
     if request.method == "GET":
-        return render(request, "login.html")
+        return render(request, "c_login.html")
     username: str = request.POST.get("username")
-    response = redirect(reverse("cst:user"))
+    response = redirect(reverse("cst:c_user"))
     # response.set_signed_cookie(COOKIE_KEYNAME, username, "salt")  # 加salt的cookie
     response.set_cookie(COOKIE_KEYNAME, username)
     return response
@@ -26,6 +28,22 @@ def cookie_logout(request):
     # request.COOKIES.pop(COOKIE_KEYNAME)
 
     # 删除浏览器的cookie
-    response = redirect(reverse("cst:login"))
+    response = redirect(reverse("cst:c_login"))
     response.delete_cookie(COOKIE_KEYNAME)
     return response
+
+
+# def session_login(request):
+#     if request.method == "GET":
+#         return render(request, "s_login.html")
+#     username: str = request.POST.get("username")
+#     request.session[SESSION_KEYNAME] = username
+#     return redirect(reverse("cst:s_user"))
+
+
+# def session_logout(request):
+#     return None
+
+
+# def session_user(request):
+#     return None
